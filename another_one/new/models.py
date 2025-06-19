3d27199a59e7293a416fc2f43a9e3dc1dbca21c6
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-06-19 11:28:49 +0300

    создал таблицу "Страна происхождения"

--- another_one/new/models.py
+++ another_one/new/models.py
@@ -1,4 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CountryOfConsignment(models.Model):
+    name = models.CharField(max_length=100)
 
 
 class MyUser(AbstractUser):
