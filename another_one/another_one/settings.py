331fa5c7f0a9f15f8c20385168c39378635c7b30
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-06-21 19:50:08 +0300

    настройка места для хранения медиа файлов
    не уверен, что делаю правильно, но как-то надо начать.

--- another_one/another_one/settings.py
+++ another_one/another_one/settings.py
@@ -137,6 +137,11 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+MEDIA_URL = 'static/images/'
+MEDIA_ROOT = [
+    BASE_DIR / 'static/images',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
