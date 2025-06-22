b8ac9cad6624a051c621ca45bed2ed381eb5328f
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-06-22 15:30:38 +0300

    исправил путь медиа файлов
    оказывается, путь в настройках обязательно должен быть строкой.
    а я всё думал как лучше указывать. вот мне и подсказали.
    в пути сохранения должна быть только следующая папка, без указания
    самой папки медиа. и да, файл нормально сохраняется в указанной папке.
    теперь мне интересно как это будет происходить на хостинге.

--- another_one/new/models.py
+++ another_one/new/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class CountryOfConsignment(models.Model):
     name = models.CharField(max_length=100)
-    state_flag = models.ImageField("Государственный флаг", upload_to="images/flag/", null=True, blank=True)
+    state_flag = models.ImageField("Государственный флаг", upload_to="flag/", null=True, blank=True)
 
 
 class City(models.Model):
