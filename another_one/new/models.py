05e8308f9dfe352d749fab0ed108298c296667b2
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-06-22 16:33:17 +0300

    адаптировал отображение моделей
    прописал МЕТА-классы и строковое представление у всех моделей.
    названия сделал уникальными, чтоб не повторялись. и включил сортировку
    по названию. а за одно прописал название приложения, чтоб
    в админ-панели всё было красиво и по русски.
    как-то много дублирования получилось. наверно надо было сделать
    один класс с полной схемой, а потом от него наследоваться.

--- another_one/new/models.py
+++ another_one/new/models.py
@@ -3,21 +3,53 @@ from django.db import models
 
 
 class CountryOfConsignment(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     state_flag = models.ImageField("Государственный флаг", upload_to="flag/", null=True, blank=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Название страны"
+        verbose_name_plural = "Названия стран"
+
+    def __str__(self):
+        return self.name
 
 
 class City(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Название города"
+        verbose_name_plural = "Названия городов"
+
+    def __str__(self):
+        return self.name
 
 
 class Profession(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Профессия"
+        verbose_name_plural = "Профессии"
+
+    def __str__(self):
+        return self.name
 
 
 class Organization(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, blank=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Организация"
+        verbose_name_plural = "Организации"
+
+    def __str__(self):
+        return self.name
 
 
 class MyUser(AbstractUser):
