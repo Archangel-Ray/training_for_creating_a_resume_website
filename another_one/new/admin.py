c309440095e8da63b9219318a192d6c18638b68e
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-06-22 15:21:22 +0300

    зарегистрировал модели в админ-панели

--- another_one/new/admin.py
+++ another_one/new/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import MyUser
+from . import models
 
-admin.site.register(MyUser)
+admin.site.register(models.MyUser)
+admin.site.register(models.CountryOfConsignment)
+admin.site.register(models.City)
+admin.site.register(models.Profession)
+admin.site.register(models.Organization)
