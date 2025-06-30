609f75a205ea193e6893c8a167375b448c9ff28f
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-06-30 22:22:58 +0300

    представление пользователя
    долго не делал фиксаций...
    это была творческая работа, не представлял себе, как и что должно
    выглядеть, и не понимал, что нужно сохранять, а что будет лишним.
    очень много раз переделывал и искал варианты.
    наконец, дошёл до этапа который можно сохранить.

--- another_one/new/views.py
+++ another_one/new/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
+from new.models import MyUser
 
 
 def main(request):
     return render(request, 'new/basic_information.html')
+
+
+class Individual(DetailView):
+    model = MyUser
+    template_name = "new/individual_information.html"
