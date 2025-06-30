9bdfef8e8f147522792dc166438aaaa73359f9e7
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-06-30 22:25:44 +0300

    путь для отображения пользователя

--- another_one/new/urls.py
+++ another_one/new/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import main
+from .views import main, Individual
 
 urlpatterns = [
     path('', main),
+    path('<int:pk>', Individual.as_view(), name="отобразить личные данные"),
 ]
