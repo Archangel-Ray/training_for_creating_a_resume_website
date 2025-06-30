70b4e395bed67892e8a5cc6c6698879e20b4d103
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-06-30 22:51:14 +0300

    путь отображения "навыков"

--- another_one/new/urls.py
+++ another_one/new/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import main, Individual
+from .views import main, Individual, skills
 
 urlpatterns = [
     path('', main),
     path('<int:pk>', Individual.as_view(), name="отобразить личные данные"),
+    path('1/data/skills', skills, name="отобразить навыки"),
 ]
