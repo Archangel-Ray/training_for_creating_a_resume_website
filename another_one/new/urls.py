6b608e9f831b4ff980679691dc8d726a0c15f35d
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-07-06 19:10:02 +0300

    прописал путь редактирования личных данных пользователя

--- another_one/new/urls.py
+++ another_one/new/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import main, Individual, skills
+from .views import main, Individual, update_individual, skills
 
 urlpatterns = [
     path('', main),
     path('<int:pk>', Individual.as_view(), name="отобразить личные данные"),
+    path('update', update_individual, name="редактировать личные данные"),
     path('1/data/skills', skills, name="отобразить навыки"),
 ]
