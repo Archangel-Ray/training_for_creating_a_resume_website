44ee8cd762defeb187837de9a9fc4048d4156eaf
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-07-06 19:01:42 +0300

    форма редактирования модели пользователя

---
+++ another_one/new/forms.py
@@ -0,0 +1,22 @@
+from django.forms import ModelForm
+
+from new.models import MyUser
+
+
+class UpdateIndividualForm(ModelForm):
+    class Meta:
+        model = MyUser
+        fields = [
+            "first_name",
+            "last_name",
+            "photo",
+            "biological_sex",
+            "birthday",
+            "citizenship",
+            "city",
+            "profession",
+            "the_level_of_professionalism",
+            "job",
+            "motto",
+            "about_me",
+        ]
