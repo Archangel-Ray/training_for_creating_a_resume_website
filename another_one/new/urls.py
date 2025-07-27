89a769f9cd394957e5298f6258c2275789095bc6
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-07-27 15:52:34 +0300

    роут отправки сообщения на почту

--- another_one/new/urls.py
+++ another_one/new/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
 
-from .views import main, Individual, update_individual, skills, transition_to_watsapp, transition_to_microsoft_teams
+from .views import (
+    main,
+    Individual,
+    update_individual,
+    skills,
+    transition_to_watsapp,
+    transition_to_microsoft_teams,
+    sending_the_message,
+)
 
 urlpatterns = [
     path('', main),
     path('<int:pk>', Individual.as_view(), name="отобразить личные данные"),
     path('update', update_individual, name="редактировать личные данные"),
     path('1/data/skills', skills, name="отобразить навыки"),
+    path('sending_the_message', sending_the_message, name="отправка сообщения с сайта мне"),
     path('watsapp', transition_to_watsapp, name="переход в ВатсАпп"),
     path('microsoft_teams', transition_to_microsoft_teams, name="переход во вражеский мессенджер"),
 ]
