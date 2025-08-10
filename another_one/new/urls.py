b3e8c2dc8bb630a38ead336602425feb0b2ef855
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-08-10 14:10:20 +0300

    прописал путь на детализированное отображение проектов

--- another_one/new/urls.py
+++ another_one/new/urls.py
@@ -13,6 +13,7 @@ from .views import (
     list_of_working,
     detailed_of_working,
     list_of_projects,
+    detailed_of_project,
 )
 
 urlpatterns = [
@@ -24,6 +25,7 @@ urlpatterns = [
     path('1/data/working', list_of_working, name="отобразить рабочие места"),
     path('1/data/working/<pk>', detailed_of_working, name="отобразить конкретное рабочее место"),
     path('1/data/project', list_of_projects, name="отобразить проекты"),
+    path('1/data/project/<pk>', detailed_of_project, name="отобразить конкретный проект"),
     path('1/data/all', skills, name="отобразить всё"),
     path('sending_the_message', sending_the_message, name="отправка сообщения с сайта мне"),
     path('watsapp', transition_to_watsapp, name="переход в ВатсАпп"),
