54dbd67c007d6ced02518f2a559c0c2a8bd4441c
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-08-16 14:53:16 +0300

    добавил поля начала и окончания в админ-панель Курсов

--- another_one/new/admin.py
+++ another_one/new/admin.py
@@ -22,6 +22,10 @@ class ProfessionAdmin(admin.ModelAdmin):
     filter_vertical = ["supplement_the_profession_of_the_user"]
 
 
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["name", "start_date", "end_date"]
+
+
 admin.site.register(MyUser)
 admin.site.register(CountryOfConsignment)
 admin.site.register(City)
@@ -32,7 +36,7 @@ admin.site.register(Language)
 admin.site.register(Skill)
 admin.site.register(Working)
 admin.site.register(Project)
-admin.site.register(Course)
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Certificate)
 admin.site.register(Passion)
 admin.site.register(DeveloperOfTheCourse)
