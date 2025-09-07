13f2dc7b50d86d65bbba4863f4a349432e46d69b
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-09-07 19:28:03 +0300

    миксин для представлений конкретных элементов модели

--- another_one/new/mixins.py
+++ another_one/new/mixins.py
@@ -25,7 +25,7 @@ class FeedbackListMixin:
         """Отзывы без object_id (относятся к разделу в целом)."""
         return self.feedback_model.objects.filter(
             content_type=self.get_feedback_content_type(),
-            object_id__isnull=True,
+            object_id__isnull=True,  # none будет означать для списка всех элементов
             status="published"
         )
 
@@ -41,13 +41,64 @@ class FeedbackListMixin:
         if form.is_valid():
             fb = form.save(commit=False)
             fb.content_type = self.get_feedback_content_type()
-            fb.object_id = None
+            fb.object_id = None  # none будет означать для списка всех элементов
             if request.user.is_authenticated:
                 fb.author_user = request.user
             fb.save()
             return redirect(request.path)
 
         # если форма невалидна рендерим с ошибками
+        context = self.get_context_data(form=form)
+        return self.render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["feedbacks"] = self.get_feedback_queryset()
+        context["feedback_form"] = kwargs.get("form", self.get_feedback_form())
+        return context
+
+
+class FeedbackDetailMixin:
+    """
+    Mixin для DetailView: добавляет блок откликов к конкретному объекту
+    (например, к одному навыку, проекту, работе, пользователю и т.д.).
+    """
+
+    feedback_model = Feedback
+    feedback_form_class = FeedbackForm
+
+    def get_feedback_content_type(self):
+        """Возвращает ContentType для конкретного элемента модели."""
+        return ContentType.objects.get_for_model(self.object)
+
+    def get_feedback_queryset(self):
+        """Отзыв на конкретный элемент конкретной модели."""
+        return self.feedback_model.objects.filter(
+            content_type=self.get_feedback_content_type(),
+            object_id=self.object.id,
+            status="published",
+        )
+
+    def get_feedback_form(self, data=None):
+        """Возвращает форму (пустую или с данными)."""
+        return self.feedback_form_class(data)
+
+    def post(self, request, *args, **kwargs):
+        """
+        Обработка формы откликов для конкретного объекта.
+        """
+        self.object = self.get_object()
+        form = self.get_feedback_form(request.POST)
+
+        if form.is_valid():
+            fb = form.save(commit=False)
+            fb.content_type = self.get_feedback_content_type()
+            fb.object_id = self.object.id
+            if request.user.is_authenticated:
+                fb.author_user = request.user
+            fb.save()
+            return redirect(request.path)
+
         context = self.get_context_data(form=form)
         return self.render_to_response(context)
 
