15bcf69e2903241c8567eaf9bf5b638c428e02fc
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-09-24 12:41:43 +0300

    добавил сигналы которые должны удалять старые фотографии пользователя
    последний штрих!
    всё, что было запланировано на сайте - готово.
    осталось несколько идей, но их я буду реализовывать по мере желания.
    перехожу к следующему этапу -> публикация на хостинге.
    я кайфую....

--- another_one/new/signals.py
+++ another_one/new/signals.py
@@ -1,9 +1,9 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
 
-from .models import Feedback
+from .models import Feedback, MyUser
 
 
 @receiver(post_save, sender=Feedback)
@@ -26,3 +26,26 @@ def notify_new_feedback(sender, instance: Feedback, created, **kwargs):
         settings.ADMINS,  # кому
         fail_silently=True,  # чтобы не падало при ошибке
     )
+
+
+@receiver(post_delete, sender=MyUser)
+def delete_file_on_object_delete(sender, instance, **kwargs):
+    """Удаление личной фотографии если пользователь выбирает "Очистить"."""
+    if instance.photo:
+        instance.photo.delete(save=False)
+
+
+@receiver(pre_save, sender=MyUser)
+def delete_old_file_on_change(sender, instance, **kwargs):
+    """Удаление личной фотографии если пользователь меняет её на другую."""
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = MyUser.objects.get(pk=instance.pk).photo
+    except MyUser.DoesNotExist:
+        return False
+
+    new_file = instance.photo
+    if old_file and old_file != new_file:
+        old_file.delete(save=False)
