fb4b59b1adf8be9926949421290f761d81acf192
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-10-08 17:21:30 +0300

    добавил модели в админ-панель

--- training_mysql/resume_app/admin.py
+++ training_mysql/resume_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import (
+    CountryOfConsignment,
+    City,
+    SupplementProfession,
+    Profession,
+    Organization,
+    Language,
+    MyUser
+)
 
-# Register your models here.
+admin.site.register(CountryOfConsignment)
+admin.site.register(City)
+admin.site.register(SupplementProfession)
+admin.site.register(Profession)
+admin.site.register(Organization)
+admin.site.register(Language)
+admin.site.register(MyUser)
