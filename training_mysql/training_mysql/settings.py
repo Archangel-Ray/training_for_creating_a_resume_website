d2b3bdb2c23b12cd194704237ae2a7a8320ee73c
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-10-08 16:46:08 +0300

    подключил приложение к проекту

--- training_mysql/training_mysql/settings.py
+++ training_mysql/training_mysql/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # приложения проекта
+    'resume_app.apps.ResumeAppConfig',
 ]
 
 MIDDLEWARE = [
